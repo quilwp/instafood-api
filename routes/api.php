9a7fee4203b55720224181b1ebfd61fd6cd65ef3
d.budancev <quil.jobs@gmail.com>
2021-12-03 22:19:27 +0300

    create base ingredient endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::resource('ingredients', App\Http\Controllers\Admin\Ingredient\IngredientController::class);
+});
