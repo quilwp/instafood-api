1a856d379bb961f298ecee788c9659957f16ca49
d.budancev <quil.jobs@gmail.com>
2021-12-05 01:25:25 +0300

    create base diets endpoint

--- app/Models/Diet.php
+++ app/Models/Diet.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Diet extends Model
 {
     use HasFactory;
+
+    /**
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'author_id');
+    }
 }
